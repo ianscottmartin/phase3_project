c15363cdd227ad59699388e7dadd21b363ecf409
Ian Scott Martin <ianscottmartin@gmail.com>
2023-08-20 14:20:59 -0600

    import column and integer

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer
 Base=declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-    Column(id)
+    id = Column("user_id", Integer, primary_key=True)
+    name = Column("user_name", String(50))
+    
