7534a127d7e4e966f662913d85dcb934cb690135
Ian Scott Martin <ianscottmartin@gmail.com>
2023-08-21 23:32:56 -0600

    alembic upgrade not working

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base=declarative_base()
+Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "users"
     id = Column("user_id", Integer, primary_key=True)
-    name = Column("user_name", String(50))
-    
+    username = Column("user_name", String(50))
