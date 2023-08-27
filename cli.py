dcb15304ef1ed72188c65fcf04c5d8760b8856d6
Ian Scott Martin <ianscottmartin@gmail.com>
2023-08-27 17:12:36 -0600

    new cli start

--- cli.py
+++ cli.py
@@ -1,28 +1,32 @@
 def start():
     print("Welcome to Comic Selector")
-    print("1. Please select(1-3)")
-    print("2.Select Comic ")
+    print("1. Select Comic")
+    print("2. ADD Comic ")
     print("3. Exit ")
     
-    user_input = input("Please Select a number(1-3)")
     
-    handle_user_input(user_input)
-    
-def handle_user_input(input):
-    is_number = input.isdigit()
-    if is_number:
-        selection = int(input)
-        if 1<= selection <= 3:
-            handle_selection(selection)
-    else:
-        print("try again")        
-        start()
+start()
     
     
+user_input = input("Please Select a number(1-3)")
+    
+   # handle_user_input(user_input)
+    
+#def handle_user_input(input):
+    #is_number = input.isdigit()
+    #if is_number:
+        #selection = int(input)
+        #if 1<= selection <= 3:
+     #       handle_selection(selection)
+    #else:
+       # print("try again")        
+       # start()
     
     
     
-    start()
+    
+    
+ 
     
 
     
