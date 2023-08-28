3ef5ea1d2bc346779cf53cf3403eeb6e6e69a86e
Ian Scott Martin <ianscottmartin@gmail.com>
2023-08-27 20:50:45 -0600

    more cli errors

--- cli.py
+++ cli.py
@@ -1,3 +1,6 @@
+from sqlalchemy.orm import Table
+
+
 def start():
     print("***Welcome to Comic Selector***")
     print("1. Select Comic")
@@ -13,13 +16,13 @@ user_input = input("Please Select a number(1-3)")
         handle_user_input(user_input)
     
 def handle_user_input(input):
-    is_number = input.isdigit()
-    if is_number:
-        selection = int(input)
+        is_number = input.isdigit()
+        if is_number:
+            selection = int(input)
         if 1<= selection <= 3:
-          handle_selection(selection)
-    else:
-       print("try again")        
+            handle_selection(selection)
+        else:
+            print("try again")        
         
 start()
     
